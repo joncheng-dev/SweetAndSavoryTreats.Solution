176b947c909cb539d3ff616b70596c116f3749d1
Jonathan Cheng <jcheng461@gmail.com>
2023-10-20 16:46:21 -0700

    add README; WIP: fix bug w successful login not showing

--- TreatsShop/Controllers/AccountController.cs
+++ TreatsShop/Controllers/AccountController.cs
@@ -36,6 +36,7 @@ namespace SweetAndSavoryTreats.Controllers
     {
       if (!ModelState.IsValid)
       {
+        ViewBag.Title = "Register a new user";
         return View(model);
       }
       else
@@ -52,9 +53,47 @@ namespace SweetAndSavoryTreats.Controllers
           {
             ModelState.AddModelError("", error.Description);
           }
+          ViewBag.Title = "Register a new user";
           return View(model);
         }
       }
+    }
+
+    public ActionResult Login()
+    {
+      ViewBag.Title = "Log in with your account";
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        ViewBag.Title = "Log in with your account";
+        return View(model);
+      }
+      else
+      {
+        Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          ViewBag.Title = "Log in with your account";
+          ModelState.AddModelError("", "There is something wrong with your email or username. Please try again.");
+          return View(model);
+        }
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> LogOff()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Index");
     }
   }
 }
