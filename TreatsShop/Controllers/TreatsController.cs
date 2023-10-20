d0a99c58d357fd3ea085e7f31709dc6046300f7f
Jonathan Cheng <jcheng461@gmail.com>
2023-10-20 09:38:01 -0700

    update treats controller index and create; corresponding views

---
+++ TreatsShop/Controllers/TreatsController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SweetAndSavoryTreats.Models;
+using System.Collections.Generic;
+using System.Linq; 
+
+namespace SweetAndSavoryTreats.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly SweetAndSavoryTreatsContext _db;
+
+    public TreatsController(SweetAndSavoryTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      ViewBag.Title = "List of Treats";
+      return View(_db.Treats.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.Title = "Add a Treat";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat newAddition)
+    {
+      _db.Treats.Add(newAddition);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
